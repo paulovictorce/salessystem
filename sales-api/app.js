d0f9d78005c6e0c238d353f6b21f6ccbd5132966
Paulo Victor Oliveira <paulo.oliveira@monitoratec.com.br>
2021-11-30 15:50:01 -0300

    Project SALES-API created.

--- sales-api/app.js
+++ sales-api/app.js
@@ -2,11 +2,11 @@ import express from "express";
 
 const app = express();
 const env = process.env;
-const PORT = env.PORT || 8080;
+const PORT = env.PORT || 8082;
 
 app.get('/api/status', (req, res) => {
     return res.status(200).json({
-        service: "AUTH-API",
+        service: "SALES-API",
         status: "UP",
         httpStatus: 200
     });
