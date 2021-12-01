df2f70171757e9095680a429f817fa8316c8040e
Paulo Victor Oliveira <paulo.oliveira@monitoratec.com.br>
2021-12-01 08:19:19 -0300

    AUTH-API: database connection configured.

---
+++ auth-api/scr/config/dbConfig.js
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize/dist";
+
+const sequelize = new Sequelize("auth-db", "admin", "postgres", {
+    host: "localhost",
+    dialect: "postgres",
+    quoteIdentifiers: false,
+    define: {
+        syncOnAssociation: true,
+        timestamps: false,
+        underscored: true,
+        underscoredAll: true,
+        freezeTableName: true
+    },
+});
+
+sequelize.authenticate().then(() => {
+    console.info("Database AUTH-DB connection has been stablished.")
+})
+.catch((err) => {
+    console.error("Unable to connect to database AUTH-DB");
+    console.error(err.message);
+});
+
+export default sequelize;
